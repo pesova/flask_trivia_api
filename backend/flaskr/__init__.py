0a28aaf3a2d227f11e31a5d83e31de1f82b5f59b
pesova <pesova13@gmail.com>
2022-09-04 07:33:38 +0100

    backend finally worked

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,6 +13,21 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
 
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
+
+    #@cross_origin
+    @app.route('/')
+    def hello_world():
+        return jsonify({'message':'HELLO, ommooooooooooooo!'})
+
+
+
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
